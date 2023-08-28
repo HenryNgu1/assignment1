d92ce682e2058bf047425ec03929ada9165d7e83
henrys <110834511+HenryNgu1@users.noreply.github.com>
2023-08-28 17:40:07 +1000

    Link to product.js page
    changes to app.js to include route to product.js added product page and change to home page added a link to access product page in grid 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import BasicGrid from './home.js'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import BasicGrid from './home.js';
+import Product from './product'; 
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <BasicGrid />
-                
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div>
+            <Router>  
+                <Routes>
+                    <Route path="/" element={<BasicGrid />} /> 
+                    <Route path="/product" element={<Product />} /> 
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
