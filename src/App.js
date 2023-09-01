0b5db6c263d42578bcef64747368fa1d526dd053
HenryNgu1 <110834511+HenryNgu1@users.noreply.github.com>
2023-09-01 17:27:53 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import BasicGrid from './home.js';
 import Product from './product'; 
+import Profile from './profilepage';
 
 
 
@@ -11,7 +12,8 @@ function App() {
             <Router>  
                 <Routes>
                     <Route path="/" element={<BasicGrid />} /> 
-                    <Route path="/product" element={<Product />} /> 
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/profilepage" element={<Profile/>}/>
                 </Routes>
             </Router>
         </div>
